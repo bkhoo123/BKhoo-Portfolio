9dc474f6b08b8dec34b7fa384dfc0c35c2842228
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2023-05-23 15:55:21 -0700

    Changes Made To Responsive Design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import a from 'next/link'
+import React, {useState} from 'react'
+import Link from 'next/link'
 import Logo from './Logo'
 import { useRouter } from 'next/router'
 import { GithubIcon } from './Icons'
@@ -11,45 +11,106 @@ const CustomLink=({href, title, className=""}) => {
 
 
     return (
-        <a href={href} className={`${className} relative group`}>
+        <Link href={href} className={`${className} relative group`}>
             {title}
 
             <span className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 
             group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full': 'w-0'}`}
             >&nbsp;</span>
-        </a>
+        </Link>
+    )
+}
+
+const CustomMobileLink=({href, title, className=""}) => {
+    const router = useRouter();
+
+
+    return (
+        <button href={href} className={`${className} relative group`}>
+            {title}
+
+            <span className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 
+            group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full': 'w-0'}`}
+            >&nbsp;</span>
+        </button>
     )
 }
 
 const NavBar = () => {
+  const [mode, setMode] = useState("")
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
-        <nav>
-            <CustomLink href="/" title="Home" className="mr-4" />
-            <CustomLink href="/about" title="Experience" className="mx-4"  />
-            <CustomLink href="/projects" title="Projects" className="mx-4"   />
-            {/* <CustomLink href="/articles" title="Articles" className="ml-4"   /> */}
-        </nav>
-        
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative">
 
-        <nav className="flex items-center justify-center flex-wrap">
-            <motion.a href="https://github.com/bkhoo123" target={"_blank"}
-            whileHover={{y:-2}}
-            className="w-6 mr-3"
-            whileTap={{scale:0.9}}
-            >
-                <GithubIcon />
-            </motion.a>
-            <motion.a href="https://www.linkedin.com/in/brian-khoo-4121b087/" target={"_blank"}
-            whileHover={{y:-2}}
-            className="w-6 mr-3 "
-            whileTap={{scale:0.9}}
-            >
-                <LinkedInIcon />
-            </motion.a>
+        <button onClick={handleClick} className="flex-col justify-center items-center hidden lg:flex">
+            <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${isOpen ? `rotate-45 translate-y-1` : `-translate-y-0.5`}`}></span>
+            <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? `opacity-0`: `opacity-100`}`}></span>
+            <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${isOpen ? `-rotate-45 -translate-y-1` : `translate-y-0.5`}`}></span>
+        </button>
 
-        </nav>
 
+        <div className="w-full flex justify-between items-center lg:hidden">
+            <nav>
+                <CustomLink href="/" title="Home" className="mr-4" />
+                <CustomLink href="/about" title="Experience" className="mx-4"  />
+                <CustomLink href="/projects" title="Projects" className="mx-4"   />
+                {/* <CustomLink href="/articles" title="Articles" className="ml-4"   /> */}
+            </nav>
+            
+
+            <nav className="flex items-center justify-center flex-wrap">
+                <motion.a href="https://github.com/bkhoo123" target={"_blank"}
+                whileHover={{y:-2}}
+                className="w-6 mr-3"
+                whileTap={{scale:0.9}}
+                >
+                    <GithubIcon />
+                </motion.a>
+                <motion.a href="https://www.linkedin.com/in/brian-khoo-4121b087/" target={"_blank"}
+                whileHover={{y:-2}}
+                className="w-6 mr-3 "
+                whileTap={{scale:0.9}}
+                >
+                    <LinkedInIcon />
+                </motion.a>
+
+            </nav>
+        </div>
+
+        <div className="min-w-[70vw] z-30 flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+        bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32
+        ">
+            <nav className="flex flex-col items-center justify-center">
+                <CustomLink href="/" title="Home" className="mr-4" />
+                <CustomLink href="/about" title="Experience" className="mx-4"  />
+                <CustomLink href="/projects" title="Projects" className="mx-4"   />
+                {/* <CustomLink href="/articles" title="Articles" className="ml-4"   /> */}
+            </nav>
+            
+
+            <nav className="flex items-center justify-center flex-wrap">
+                <motion.a href="https://github.com/bkhoo123" target={"_blank"}
+                whileHover={{y:-2}}
+                className="w-6 mr-3"
+                whileTap={{scale:0.9}}
+                >
+                    <GithubIcon />
+                </motion.a>
+                <motion.a href="https://www.linkedin.com/in/brian-khoo-4121b087/" target={"_blank"}
+                whileHover={{y:-2}}
+                className="w-6 mr-3 "
+                whileTap={{scale:0.9}}
+                >
+                    <LinkedInIcon />
+                </motion.a>
+
+            </nav>
+        </div>
 
         <div className="absolute left-[50%] top-2 translates-x-[-50%]">
         <Logo />
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,14 @@ module.exports = {
         circularLight: 'repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 100px);'
       }
     },
+    screens: {
+      "2xl": {max: "1535px"},
+      "xl": {max: "1279px"},
+      "lg": {max: "1023px"},
+      "md": {max: "767px"},
+      "sm": {max: "639px"},
+      "xs": {max: "479px"},
+    },
   },
   plugins: [],
 }
